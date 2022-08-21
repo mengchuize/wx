2da7e061dba54aa7e2ec163aecf7b45a7c2abe30
孟垂泽 <56497625+mengchuize@users.noreply.github.com>
2022-08-22 00:03:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,7 @@ def get_weather():
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
+def get_days():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
@@ -44,14 +40,22 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 today_str =  str(today.year) + "年" + str(today.month) + "月" + str(today.day) + "日"
+weather_str, temputer_str = get_weather()
+days_str = get_days()
+copyright_str = "[这里的信息仅你可见]"
+others1_str = ""
+others2_str = ""
+
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-# wea, temperature = get_weather()
-# data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-data = {"message0":{"value":"“跟你在一起的时光，全都很耀眼。因为天气好，因为天气不好，因为天气刚刚好。每一天，都很美好。”"},
-        "message1":{"value":"    这是我在这里发给你的第一个信息，很开心可以与你相识，更希望我们可以相伴很久。以后就在这里和你说早安啦！"},
-        "time":{"value":today_str,"color":"#C0C0C0"},
-        "copyright":{"value":"[这里的信息仅你可见]","color":"#F08080"}}
+
+data = {"time":{"value":today_str,"color":"#C0C0C0"},
+        "weather":{"value":weather_str},
+        "temputer":{"value":temputer_str},
+        "days":{"value":days_str},
+        "others1":{"value":others1_str},
+        "others2":{"value":others2_str},
+        "copyright":{"value":copyright_str},"color":"#F08080"}}
+
 res = wm.send_template(user_id, template_id, data)
-print(res)
